ecc4baf33561c2f270cfebfb4853a77be78a7d07
mbats254 <mbatiacisco@gmail.com>
2023-06-17 17:24:17 +0300

    ifmcltd

--- resources/views/sites_files/footers.blade.php
+++ resources/views/sites_files/footers.blade.php
@@ -82,5 +82,209 @@
                 </div>
             </div>
 </nav>
+ <!--loader-->
+ <div class="loader_all" id="loader_all">
+    <div class="l_any">
+        
+        <img src="/img/lder/l3.gif" alt="">
+        
+    </div>
+</div>
 
 @yield('content')
+<footer>
+    <div class="hfooter">
+        <h3>Quick Links</h3>
+        <li><a href="/" class="home Hf"><i class="fas fa-long-arrow-right"></i>Home</a></li>
+        <li><a href="/about" class="about Af"><i class="fas fa-long-arrow-right"></i>About</a></li>
+        <li><a href="/service" class="service Sf"><i class="fas fa-long-arrow-right"></i>Services</a></li>
+        <li><a href="/videos" class="international If"><i class="fas fa-long-arrow-right"></i>Videos</a></li>
+        <li><a href="/news" class="news Nf"><i class="fas fa-long-arrow-right"></i>News</a></li>
+        <li><a href="/contact" class="contact Cf"><i class="fas fa-long-arrow-right"></i>Contact</a></li>
+    </div>
+    <div class="hfooter">
+        <h3>Our Services</h3>
+        <li><a href="/servicefinance" class="CAbtn"><i class="fas fa-long-arrow-right"></i>Finance Management</a></li>
+        <li><a href="/serviceoperation" class="TSbtn"><i class="fas fa-long-arrow-right"></i>Operation Management</a></li>
+        <li><a href="/servicestrategic" class="OSbtn"><i class="fas fa-long-arrow-right"></i>Strategic Management</a></li>
+        <li><a href="/serviceinstitutionleakage" class=" AAbtn"><i class="fas fa-long-arrow-right"></i>Finance Literacy</a></li>
+    </div>
+    <div class="hfooter">
+        <h3>Extras</h3>
+        <li><a href="#"><i class="fas fa-long-arrow-right"></i>Privacy Policy</a></li>
+        <li><a href="#"><i class="fas fa-long-arrow-right"></i>Help</a></li>
+        <li><a href="#"><i class="fas fa-long-arrow-right"></i>Career</a></li>
+        <li><a href="#"><i class="fas fa-long-arrow-right"></i>Terms & Conditions</a></li>
+    </div>
+    <div class="hfooter">
+        <h3>Get in Touch</h3>
+        <li>P.O. Box 1668, 00900<br><br>Nairobi,<br><br>Kenya</li>
+        <li>Tel: +254768072844</li>
+        <li>Email: info@ifmcltd.com</li>
+
+    </div>
+    <div class="hfooter">
+        <h3>Newsletter</h3>
+        <p>Get E-mail updates about our services and special offers, </p>
+        <div class="subscribe">
+            <input type="email" placeholder="Example@gmail.com" required>
+            <a href="#" class="yellow">SUBSCRIBE</a>
+        </div>
+    </div>
+    <div class="Payments">
+        <h3>Secured Payment Gateways</h3>
+        <div class="p-logo">
+            <li><img src="/img/paylog/logo3.webp" alt="" width="60" height="60"></li>
+            <li><img src="/img/paylog/logom.png" alt="" width="60" height="60"></li>
+            <li><img src="/img/paylog/logok.webp" alt="" width="60" height="60"></li>
+            <li><img src="/img/paylog/logo1.png" alt="" width="60" height="60"></li>
+        </div>
+    </div>
+    <div class="copyrighticons">
+        <p>Copyright © 2022 All rights reserved | This template created by Mwaura Kennedy</p>
+        <div class="f-links">
+            <a href="#" class="center">
+                <i class="fab fa-facebook-f"></i>
+            </a>
+            <a href="#" class="center">
+                <i class="fab fa-instagram "></i>
+            </a>
+            <a href="#" class="center">
+                <i class="fab fa-linkedin"></i>
+            </a>
+            <a href="#" class="center">
+                <i class="fab fa-twitter"></i>
+            </a>
+            <a href="#" class="center">
+                <i class="fab fa-youtube"></i>
+            </a>
+        </div>
+    </div>
+</footer>
+<script src="/js/jQuery.js"></script>
+<script src="/js/triggerscript.js"></script>
+<script src="/js/owl.carousel.min.js"></script>
+<script type="text/javascript">
+      /*-------bookform------*/
+      $(document).on('click', '.bsbtn', function () {
+        $('.bookform').addClass('bookfin-active')
+    });
+    $(document).on('click', '.b-out', function () {
+        $('.bookform').removeClass('bookfin-active')
+    });
+    //chat
+    $(document).on('click', '.online_chat', function () {
+        $('.o-chatdisplay').addClass('comethou')
+    });
+    $(document).on('click', '.chat-cancel,.notification,.Search', function () {
+        $('.o-chatdisplay').removeClass('comethou')
+    });
+      //reveal
+      window.addEventListener('scroll', reveal);
+    function reveal() {
+        var reveals = document.querySelectorAll(".reveal");
+        for (var t = 0; t < reveals.length; t++) {
+            var windowheight = window.innerHeight;
+            var revealtop = reveals[t].getBoundingClientRect().top;
+            var revealpoint = 74;
+            if (revealtop < windowheight - revealpoint) {
+                reveals[t].classList.add('ona');
+            }
+            else {
+                reveals[t].classList.remove('ona');
+            }
+        }
+    }
+    window.addEventListener('scroll', lef);
+    function lef() {
+        var lefts = document.querySelectorAll(".anileft");
+        for (var l = 0; l < lefts.length; l++) {
+            var windowheight = window.innerHeight;
+            var revealtop = lefts[l].getBoundingClientRect().top;
+            var revealpoint = 110;
+            if (revealtop < windowheight - revealpoint) {
+                lefts[l].classList.add('leta');
+            }
+            else {
+                lefts[l].classList.remove('leta');
+            }
+        }
+    }
+    window.addEventListener('scroll', righ);
+    function righ() {
+        var rights = document.querySelectorAll(".aniright");
+        for (var r = 0; r < rights.length; r++) {
+            var windowheight = window.innerHeight;
+            var revealtop = rights[r].getBoundingClientRect().top;
+            var revealpoint = 140;
+            if (revealtop < windowheight - revealpoint) {
+                rights[r].classList.add('itisha');
+            }
+            else {
+                rights[r].classList.remove('itisha');
+            }
+        }
+    }
+    window.addEventListener('scroll', botto);
+    function botto() {
+        var downs = document.querySelectorAll(".anibottom");
+        for (var b = 0; b < downs.length; b++) {
+            var windowheight = window.innerHeight;
+            var revealtop = downs[b].getBoundingClientRect().top;
+            var revealpoint = 74;
+            if (revealtop < windowheight - revealpoint) {
+                downs[b].classList.add('uptop');
+            }
+            else {
+                downs[b].classList.remove('uptop');
+            }
+        }
+    }
+    window.addEventListener('scroll', uleft);
+    function uleft() {
+        var ulefts = document.querySelectorAll(".uanileft");
+        for (var pl = 0; pl < ulefts.length; pl++) {
+            var windowheight = window.innerHeight;
+            var revealtop = ulefts[pl].getBoundingClientRect().top;
+            var revealpoint = 50;
+            if (revealtop < windowheight - revealpoint) {
+                ulefts[pl].classList.add('gleft');
+            }
+            else {
+                ulefts[pl].classList.remove('gleft');
+            }
+        }
+    }
+    window.addEventListener('scroll', uright);
+    function uright() {
+        var urights = document.querySelectorAll(".uaniright");
+        for (var pr = 0; pr < urights.length; pr++) {
+            var windowheight = window.innerHeight;
+            var revealtop = urights[pr].getBoundingClientRect().top;
+            var revealpoint = 50;
+            if (revealtop < windowheight - revealpoint) {
+                urights[pr].classList.add('gright');
+            }
+            else {
+                urights[pr].classList.remove('gright');
+            }
+        }
+    }
+    window.addEventListener('scroll', ileft);
+    function ileft() {
+        var ilefts = document.querySelectorAll(".kuanileft");
+        for (var pp = 0; pp < ilefts.length; pp++) {
+            var windowheight = window.innerHeight;
+            var revealtop = ilefts[pp].getBoundingClientRect().top;
+            var revealpoint = 50;
+            if (revealtop < windowheight - revealpoint) {
+                ilefts[pp].classList.add('lleft');
+            }
+            else {
+                ilefts[pp].classList.remove('lleft');
+            }
+        }
+    }
+</script>
+</body>
+</html>
