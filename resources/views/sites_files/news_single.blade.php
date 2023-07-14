9dc9293c656967a8d58abe8d24c91096eec6a5ed
Mwaura Kennedy N <kennedymwaura155@gmail.com>
2023-07-14 03:02:11 +0300

    change news_single script added

--- resources/views/sites_files/news_single.blade.php
+++ resources/views/sites_files/news_single.blade.php
@@ -279,5 +279,93 @@
             </section>
             </section>
 </section>
+<script>
+     //next & Previous
+     var ondisplayDiv = 0;
+        function showPage() {
+            $(".subject-kaiga").hide();
+            $(".subject-kaiga:eq(" + ondisplayDiv + ")").show();
+        }
+        showPage()
+        var dateDisplay = 0;
+        function datePage() {
+            $(".recent-kaiga-details").hide();
+            $(".recent-kaiga-details:eq(" + dateDisplay + ")").show();
+        }
+        datePage()
+        var commentsDisplay = 0;
+        function commentPage() {
+            $(".rcomments-kaiga-details").hide();
+            $(".rcomments-kaiga-details:eq(" + commentsDisplay + ")").show();
+        }
+        commentPage()
+        var relatedDisplay = 0;
+        function relatedPage() {
+            $(".related-kaiga-body").hide();
+            $(".related-kaiga-body:eq(" + relatedDisplay + ")").show();
+        }
+        relatedPage()
+        
+        function bringNext() {
+            if (ondisplayDiv == $(".subject-kaiga").length - 1) {
+                ondisplayDiv = 0;
+            }
+            else {
+                ondisplayDiv++;
+            }
+            showPage();
+            if (dateDisplay == $(".recent-kaiga-details").length - 1) {
+                dateDisplay = 0;
+            }
+            else {
+                dateDisplay++;
+            }
+            datePage();
+            if (commentsDisplay == $(".rcomments-kaiga-details").length - 1) {
+                commentsDisplay = 0;
+            }
+            else {
+                commentsDisplay++;
+            }
+            commentPage();
+            if (relatedDisplay == $(".related-kaiga-body").length - 1) {
+                relatedDisplay = 0;
+            }
+            else {
+                relatedDisplay++;
+            }
+            relatedPage();
+        }
+        function bringPrev() {
+            if (ondisplayDiv == 0) {
+                ondisplayDiv = $(".subject-kaiga").length - 1;
+            }
+            else {
+                ondisplayDiv--;
+            }
+            showPage();
+            if (dateDisplay == 0) {
+                dateDisplay = $(".recent-kaiga-details").length - 1;
+            }
+            else {
+                dateDisplay--;
+            }
+            datePage();
+            if (commentsDisplay == 0) {
+                commentsDisplay = $(".rcomments-kaiga-details").length - 1;
+            }
+            else {
+                commentsDisplay--;
+            }
+            commentPage();
+            if (relatedDisplay == 0) {
+                relatedDisplay = $(".related-kaiga-body").length - 1;
+            }
+            else {
+                relatedDisplay--;
+            }
+            relatedPage();
+        }
+</script>
 
 @endsection
